5891a01ec316d56d9bdd13818a955c722f28e2ff
Konosuke Watanabe <trevelink.zelda113@gmail.com>
2026-02-28 23:34:41 +0900

    feat: GitHubサインイン用ボタン作成

---
+++ src/app/signin/_components/github-button.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import Image from 'next/image';
+
+import { authClient } from '@/lib/better-auth/auth-client';
+
+export function GitHubButton() {
+  const signIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: 'github',
+    });
+    console.log('Sign-in response:', data);
+  };
+
+  return (
+    <button
+      className="flex gap-x-2 items-center bg-black dark:bg-white hover:bg-gray-700 dark:hover:bg-gray-200 text-white dark:text-black font-bold py-2 px-3 rounded focus:outline-none focus:shadow-outline"
+      type="button"
+      onClick={signIn}
+    >
+      <Image
+        className="inline-block invert dark:invert-0 rounded-sm"
+        src="/github-svgrepo-com.svg"
+        alt="GitHub Logo"
+        width={20}
+        height={20}
+      />
+      Sign In with GitHub
+    </button>
+  );
+}
